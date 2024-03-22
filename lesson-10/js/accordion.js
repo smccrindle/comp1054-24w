8a096bfb465807aabfa0d5a48975ee73ee600f21
Scott McCrindle <scott@mccrindle.me>
2024-03-22 08:21:06 -0400

    Adding lesson 10 start files

---
+++ lesson-10/js/accordion.js
@@ -0,0 +1,14 @@
+// STEP 6: Nab all the SUMMARY elements
+
+
+// STEP 7: Loop through the element array to add an event listener, and reference a function called closeOpenDetails
+
+
+// STEP 8a: Create the closeOpenDetails() function
+
+	// STEP 8b: Loop through the summaries array again
+
+		// STEp 8c: Check to make sure the DETAILS element is not the parent of the SUMMARY that was clicked 
+
+
+// STEP 9: Check in the browser to make sure there are no errors - use the console, then proceed to the CSS for the final step
