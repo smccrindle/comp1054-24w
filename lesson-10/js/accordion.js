990f32e7c8fcb723daca0bf5983f9b0883131a04
Scott McCrindle <scott@mccrindle.me>
2024-03-25 19:37:51 -0400

    Changed a few things with Lab 3 demonstration code, and completed CSS for lesson

--- lesson-10/js/accordion.js
+++ lesson-10/js/accordion.js
@@ -1,14 +1,21 @@
 // STEP 6: Nab all the SUMMARY elements
-
+const summaries = document.querySelectorAll("summary");
 
 // STEP 7: Loop through the element array to add an event listener, and reference a function called closeOpenDetails
-
+summaries.forEach((summary) => {
+	summary.addEventListener("click", closeOpenDetails)
+});
 
 // STEP 8a: Create the closeOpenDetails() function
-
+function closeOpenDetails() {
+	//console.log("function called!");
 	// STEP 8b: Loop through the summaries array again
-
-		// STEp 8c: Check to make sure the DETAILS element is not the parent of the SUMMARY that was clicked 
-
-
+	summaries.forEach((summary) => {
+		// STEP 8c: Check to make sure the DETAILS element is not the parent of the SUMMARY that was clicked 
+		let detail = summary.parentNode;
+		if (detail != this.parentNode) {
+			detail.removeAttribute("open");
+		}
+	});
+}
 // STEP 9: Check in the browser to make sure there are no errors - use the console, then proceed to the CSS for the final step
